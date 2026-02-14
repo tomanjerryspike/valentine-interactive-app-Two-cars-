536026cf840c258cc690058be14e911f1464b6c2
tomanjerryspike <tomanjerryspike@gmail.com>
2026-02-14 03:42:57 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -153,7 +153,7 @@ function vitePluginManusDebugCollector(): Plugin {
 const plugins = [react(), tailwindcss(), jsxLocPlugin(), vitePluginManusRuntime(), vitePluginManusDebugCollector()];
 
 export default defineConfig({
-  base: "/valentine-interactive-app-Two-cars-/",
+  base: "./",
   plugins,
   resolve: {
     alias: {
